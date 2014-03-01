191707c0470b172596a2aa6c1b59bd896fa7dba7
David L <david.lindblad@hotmail.com>
2014-03-01 20:21:09 +0100

    Initial

---
+++ ChutzpahDemo001/Controllers/LevenshteinController.cs
@@ -0,0 +1,37 @@
+﻿namespace ChutzpahDemo001.Controllers
+{
+    using System;
+    using System.Web.Http;
+
+    public class LevenshteinController : ApiController
+    {
+        // GET api/values/5
+        public int Get(string value1, string value2)
+        {
+            var value1Length = value1.Length;
+            var value2Length = value2.Length;
+            var diffArray = new int[value1Length + 1, value2Length + 1];
+
+            for (var i = 0; i <= value1Length; diffArray[i, 0] = i++)
+            {
+            }
+
+            for (var j = 0; j <= value2Length; diffArray[0, j] = j++)
+            {
+            }
+
+            for (var i = 1; i <= value1Length; i++)
+            {
+                for (var j = 1; j <= value2Length; j++)
+                {
+                    var cost = (value2[j - 1] == value1[i - 1]) ? 0 : 1;
+
+                    diffArray[i, j] = Math.Min(
+                        Math.Min(diffArray[i - 1, j] + 1, diffArray[i, j - 1] + 1),
+                        diffArray[i - 1, j - 1] + cost);
+                }
+            }
+            return diffArray[value1Length, value2Length];
+        }
+    }
+}
---
+++ ChutzpahDemo001/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChutzpahDemo001.WebApiApplication" Language="C#" %>
