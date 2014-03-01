d4ab84f44eac437cb20d41601e4132b1b1af12d3
David L <david.lindblad@hotmail.com>
2014-03-01 22:37:31 +0100

    something

---
+++ ChutzpahDemo001.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,22 @@
+﻿namespace ChutzpahDemo001.Tests.Controllers
+{
+    using ChutzpahDemo001.Controllers;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LevenshteinControllerTest
+    {
+        [TestMethod]
+        public void GetLevenshtein()
+        {
+            // Arrange
+            var controller = new LevenshteinController();
+
+            Assert.AreEqual(1, controller.Get("David", "Davvid"));
+            Assert.AreEqual(2, controller.Get("David", "Divad"));
+            Assert.AreEqual(1, controller.Get("David", "Dvid"));
+            Assert.AreEqual(2, controller.Get("Katrine", "Ktrne"));
+        }
+    }
+}
